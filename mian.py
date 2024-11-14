8e125d61553bd5b287ec1eca0399c93aaca4c5e6
Blop <claire.gestalt@gmail.com>
2024-11-14 13:48:56 +0300

    Everything now generated via Faker Courier added

--- mian.py
+++ mian.py
@@ -1,33 +1,40 @@
 import pg8000.native
+from faker.proxy import Faker
+
 from generators import Gen
 import random
+import faker
 
 con = pg8000.native.Connection("postgres", password="123123")
 
-with open('names.txt', 'r') as names, open('surnames1.txt', 'r') as surnames, open('cities.txt', 'r') as cities, open(
-        'addresses.txt', 'r') as addresses:
-    names = names.readlines()
-    surnames = surnames.readlines()
-    cities = cities.readlines()
-    addresses = addresses.readlines()
-    gen = Gen()
-    #Generate main data
-    for _ in range(10 ** 3):
-        con.run(
-            f'''INSERT INTO yandex_eats_ph.person (phone_number, full_name, login, password) VALUES ({gen.gen_phone_number()},'{gen.gen_fullname(names, surnames)}','{gen.gen_seq()}', '{gen.gen_pass()}')''')
+gen = Faker()
+transport = ['On_foot', 'Bicycle', 'Car', 'Helicopter']
+# Generate main data
+for _ in range(10 ** 3):
+    con.run(
+        f'''INSERT INTO yandex_eats_ph.person (phone_number, full_name, login, password) VALUES ({int(gen.basic_phone_number().replace('-', '').replace('(', '').replace(')', ''))},'{gen.name()}','{gen.aba()}', '{gen.aba().__hash__()}')''')
 
 
-    #Generate additional data
-    for _ in range(10 ** 3):
-        con.run(
-            f"INSERT INTO yandex_eats_ph.address (city, street, house, entrance) VALUES ('{gen.gen_city(cities)}',  '{gen.gen_address(addresses)}','{gen.gen_number(2)}', {gen.gen_number(2)})")
+# Generate additional data
 
 
-    #Fill relations
-    user_ids = con.run(f"SELECT person_id FROM yandex_eats_ph.person")
-    address_ids = con.run(f"SELECT address_id FROM yandex_eats_ph.address")
-    for user_id in user_ids:
-        con.run(f"insert into yandex_eats_ph.change_address (person_id, address_id) values ({user_id[0]}, {random.choice(address_ids)[0]})")
+for _ in range(10 ** 3):
+    con.run(
+        f"INSERT INTO yandex_eats_ph.address (city, street, house, entrance) VALUES ('{gen.city()}',  '{gen.address().replace('\n', ' ')}','{gen.numerify('%%')}', {gen.numerify('%%')})")
+    con.run(
+        f"INSERT INTO yandex_eats_ph.courier (name, rating, transport, is_busy) VALUES ('{gen.name()}',  {float(gen.numerify('#.##')) % 5},'{random.choice(transport)}', {random.choice([True, False])})")
+
+# Fill relations
+
+
+user_ids = con.run(f"SELECT person_id FROM yandex_eats_ph.person")
+address_ids = con.run(f"SELECT address_id FROM yandex_eats_ph.address")
+for user_id in user_ids:
+    con.run(
+        f"insert into yandex_eats_ph.change_address (person_id, address_id) values ({user_id[0]}, {random.choice(address_ids)[0]})")
+
+
+
 
 n = 10
 for row in con.run(f"SELECT * FROM yandex_eats_ph.person limit {n}"):
